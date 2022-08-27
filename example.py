52b3eedb228c86329f27c63b1b02ea25724bcc38
Ruta Reisoglu <rutareiso@protonmail.com>
2022-08-26 23:46:02 -0400

    Start learning

---
+++ example.py
@@ -0,0 +1,14 @@
+print(1)
+print("keee")
+print(2*6)
+print("kkw" + " ll " + str(7.5))
+print(f"kkw ll  {7.5}")  # f - format
+to_seconds = 24 * 60 * 60
+
+
+def fun_function():
+    print("hey")
+    print("hey")
+
+
+fun_function()
